84dc5170ca73a5f4b1f0d2dd9ae16108534fe619
Alban Thaumur <th.alban@outlook.fr>
2023-05-29 15:41:51 +0200

    :sparkles: Add a Course module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { DatabaseModule } from './modules/config/database/database.module';
+import { CourseModule } from './modules/course/course.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, CourseModule],
   controllers: [AppController],
   providers: [],
 })
