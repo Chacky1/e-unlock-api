3a11891bc354f1c4a94702e5181a58670e377057
Alban Thaumur <th.alban@outlook.fr>
2023-05-29 15:58:01 +0200

    :sparkles: Add SectionRepository with a createSection method

--- src/modules/course/course.module.ts
+++ src/modules/course/course.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CourseRepository } from './repositories/course.repository';
+import { SectionRepository } from './repositories/section.repository';
 
 @Module({
   imports: [],
   controllers: [],
-  providers: [CourseRepository],
+  providers: [CourseRepository, SectionRepository],
 })
 export class CourseModule {}
---
+++ src/modules/course/repositories/section.repository.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { Section } from '@prisma/client';
+import { DatabaseService } from 'src/modules/config/database/providers/services/database.service';
+import { CreateSectionDto } from '../dto/create-section.dto';
+
+@Injectable()
+export class SectionRepository {
+  public constructor(private readonly databaseService: DatabaseService) {}
+
+  public async createSection(
+    createSectionDto: CreateSectionDto,
+  ): Promise<Section> {
+    const section = await this.databaseService.section.create({
+      data: createSectionDto,
+    });
+
+    return section;
+  }
+}
