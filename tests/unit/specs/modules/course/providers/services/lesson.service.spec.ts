2ec550b904ab3dcbd8dfb9a54f19d20c44d54399
Alban Thaumur <th.alban@outlook.fr>
2023-05-29 17:37:48 +0200

    :white_check_mark: Test createLesson with a simple test

---
+++ tests/unit/specs/modules/course/providers/services/lesson.service.spec.ts
@@ -0,0 +1,38 @@
+import { Test } from '@nestjs/testing';
+import { LessonRepository } from '../../../../../../../src/modules/course/providers/repositories/lesson.repository';
+import { LessonService } from '../../../../../../../src/modules/course/providers/services/lesson.service';
+import { createFakeLessonDto } from '../../../../../../factories/course/dto/create-course/create-lesson.dto.factory';
+
+describe('Lesson Service', () => {
+  let service;
+  let repository;
+
+  const lessonRepositoryMock = {
+    createLesson: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        LessonService,
+        {
+          provide: LessonRepository,
+          useValue: lessonRepositoryMock,
+        },
+      ],
+    }).compile();
+
+    service = module.get<LessonService>(LessonService);
+    repository = module.get<LessonRepository>(LessonRepository);
+  });
+
+  describe('createLesson', () => {
+    it('should create a lesson when called.', async () => {
+      const toCreateLesson = createFakeLessonDto();
+
+      await service.createLesson(toCreateLesson);
+
+      expect(repository.createLesson).toHaveBeenCalledWith(toCreateLesson);
+    });
+  });
+});
