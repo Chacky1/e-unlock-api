4dee1c08c45a3ef65cbaad04898211b377451036
Alban Thaumur <th.alban@outlook.fr>
2023-05-29 15:58:56 +0200

    :sparkles: Add LessonRepository with a createLesson method

--- src/modules/course/course.module.ts
+++ src/modules/course/course.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CourseRepository } from './repositories/course.repository';
 import { SectionRepository } from './repositories/section.repository';
+import { LessonRepository } from './repositories/lesson.repository';
 
 @Module({
   imports: [],
   controllers: [],
-  providers: [CourseRepository, SectionRepository],
+  providers: [CourseRepository, SectionRepository, LessonRepository],
 })
 export class CourseModule {}
---
+++ src/modules/course/repositories/lesson.repository.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { Lesson } from '@prisma/client';
+import { DatabaseService } from 'src/modules/config/database/providers/services/database.service';
+import { CreateLessonDto } from '../dto/create-lesson.dto';
+
+@Injectable()
+export class LessonRepository {
+  public constructor(private readonly databaseService: DatabaseService) {}
+
+  public async createLesson(createLessonDto: CreateLessonDto): Promise<Lesson> {
+    const lesson = await this.databaseService.lesson.create({
+      data: createLessonDto,
+    });
+
+    return lesson;
+  }
+}
