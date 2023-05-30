e2f4df4df3a73102a16592ef3c683d7efac62c73
Alban Thaumur <th.alban@outlook.fr>
2023-05-30 11:04:21 +0200

    :heavy_plus_sign: Add test on POST /course when name is too short

--- tests/e2e/specs/modules/course/controllers/course.controller.e2e-spec.ts
+++ tests/e2e/specs/modules/course/controllers/course.controller.e2e-spec.ts
@@ -1,19 +1,22 @@
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
 import { CourseModule } from '../../../../../../src/modules/course/course.module';
 import { createFakeCourseDto } from '../../../../../factories/course/dto/create-course/create-course.dto.factory';
+import { CreateCourseDto } from '../../../../../../src/modules/course/dto/create-course.dto';
+import { initNestApp } from '../../../../../e2e/helpers/nest-app.helper';
 
 describe('Course Controller (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       imports: [CourseModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
+    app = module.createNestApplication();
+
+    await initNestApp(app);
   });
 
   afterAll(async () => {
@@ -21,6 +24,21 @@ describe('Course Controller (e2e)', () => {
   });
 
   describe('POST /courses', () => {
+    it.each<{ payload: CreateCourseDto; test: string; errorMessage: string }>([
+      {
+        payload: createFakeCourseDto({}, { name: '' }),
+        test: 'should return 400 when name is empty.',
+        errorMessage: 'name must be longer than or equal to 1 characters',
+      },
+    ])('$test', async ({ payload, errorMessage }) => {
+      const response = await request(app.getHttpServer())
+        .post('/courses')
+        .send(payload)
+        .expect(HttpStatus.BAD_REQUEST);
+
+      expect(response.body.message[0]).toEqual(errorMessage);
+    });
+
     it('should create a course when called.', async () => {
       const toCreateCourse = createFakeCourseDto();
 
