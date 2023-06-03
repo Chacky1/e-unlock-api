2220eaf37c95c9e76bf6680da0be702b3927bce0
Alban Thaumur <th.alban@outlook.fr>
2023-06-03 13:23:47 +0200

    :sparkles: Add overriding possibility to lesson faker factory

--- tests/factories/course/dto/create-course/create-lesson.dto.factory.ts
+++ tests/factories/course/dto/create-course/create-lesson.dto.factory.ts
@@ -1,10 +1,21 @@
 import { faker } from '@faker-js/faker';
 import { CreateLessonDto } from '../../../../../src/modules/course/dto/create-lesson.dto';
 
-export const createFakeLessonDto = (lesson: Partial<CreateLessonDto> = {}) => ({
+export const createFakeLessonDto = (
+  lesson: Partial<CreateLessonDto> = {},
+  override?: Partial<CreateLessonDto>,
+) => ({
   name: lesson.name ?? faker.lorem.word(),
-  textContent: lesson.textContent ?? faker.lorem.paragraph(),
-  sectionId: lesson.sectionId ?? faker.number.int(),
-  sectionOrder: lesson.sectionOrder ?? faker.number.int(),
-  active: lesson.active ?? faker.datatype.boolean(),
+  textContent: lesson.textContent ?? faker.lorem.paragraph(3),
+  sectionId:
+    lesson.sectionId ??
+    faker.number.int({
+      max: 1000,
+    }),
+  sectionOrder:
+    lesson.sectionOrder ??
+    faker.number.int({
+      max: 1000,
+    }),
+  ...override,
 });
