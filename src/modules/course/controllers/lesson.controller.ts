84750650ccbdb2870caec36b4eacd730e4b6b7f2
Alban Thaumur <th.alban@outlook.fr>
2023-06-03 13:13:44 +0200

    :sparkles: Add ErrorsInterceptor to create lessons route
    In order to handle cases when section id does not exist

--- src/modules/course/controllers/lesson.controller.ts
+++ src/modules/course/controllers/lesson.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseInterceptors } from '@nestjs/common';
 import { LessonService } from '../providers/services/lesson.service';
 import { CreateLessonDto } from '../dto/create-lesson.dto';
+import { ErrorsInterceptor } from '../providers/interceptors/errors.interceptor';
 
 @Controller('lessons')
 export class LessonController {
   public constructor(private readonly lessonService: LessonService) {}
 
   @Post()
+  @UseInterceptors(ErrorsInterceptor)
   public async create(@Body() createLessonDto: CreateLessonDto) {
     return await this.lessonService.create(createLessonDto);
   }
