081f1f54693387486672ed5e7fef3d29a06205ac
Alban Thaumur <th.alban@outlook.fr>
2023-06-08 13:17:04 +0200

    :recycle: Refactored e2e sections controller tests

--- tests/e2e/specs/modules/course/controllers/section.controller.e2e-spec.ts
+++ tests/e2e/specs/modules/course/controllers/section.controller.e2e-spec.ts
@@ -5,9 +5,12 @@ import { CourseModule } from '../../../../../../src/modules/course/course.module
 import { initNestApp } from '../../../../../e2e/helpers/nest-app.helper';
 import { createFakeSectionDto } from '../../../../../factories/course/dto/create-course/create-section.dto.factory';
 import { CreateSectionDto } from '../../../../../../src/modules/course/dto/create-section.dto';
+import { createFakeCourseDto } from '../../../../../factories/course/dto/create-course/create-course.dto.factory';
 
 describe('Section Controller', () => {
   let app: INestApplication;
+  let existingCourseId: number;
+  const unknownCourseId = 999;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -17,6 +20,14 @@ describe('Section Controller', () => {
     app = module.createNestApplication();
 
     await initNestApp(app);
+
+    const fakeCourse = createFakeCourseDto();
+
+    const courseResponse = await request(app.getHttpServer())
+      .post('/courses')
+      .send(fakeCourse);
+
+    existingCourseId = courseResponse.body.id;
   });
 
   afterAll(async () => {
@@ -30,14 +41,17 @@ describe('Section Controller', () => {
       errorMessage: string;
     }>([
       {
-        payload: createFakeSectionDto({ courseId: 1 }, { name: '' }),
+        payload: createFakeSectionDto(
+          { courseId: existingCourseId },
+          { name: '' },
+        ),
         test: 'should return 400 when name is empty.',
         errorMessage: 'name must be longer than or equal to 1 characters.',
       },
       {
-        payload: createFakeSectionDto({ courseId: 999 }),
+        payload: createFakeSectionDto({ courseId: unknownCourseId }),
         test: 'should return 400 when course id does not exist.',
-        errorMessage: 'courseId not found.',
+        errorMessage: 'course_id not found.',
       },
     ])('$test', async ({ payload, errorMessage }) => {
       const response = await request(app.getHttpServer())
@@ -53,7 +67,9 @@ describe('Section Controller', () => {
     });
 
     it('should create a section when called.', async () => {
-      const toCreateSection = createFakeSectionDto({ courseId: 1 });
+      const toCreateSection = createFakeSectionDto({
+        courseId: existingCourseId,
+      });
 
       await request(app.getHttpServer())
         .post('/sections')
