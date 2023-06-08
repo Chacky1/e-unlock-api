2fd386ab0bb437ee403e87dc13c2e64ab6acc10a
Alban Thaumur <th.alban@outlook.fr>
2023-06-08 13:17:14 +0200

    :recycle: Refactored e2e lessons controller tests

--- tests/e2e/specs/modules/course/controllers/lesson.controller.e2e-spec.ts
+++ tests/e2e/specs/modules/course/controllers/lesson.controller.e2e-spec.ts
@@ -5,9 +5,16 @@ import { CourseModule } from '../../../../../../src/modules/course/course.module
 import { CreateLessonDto } from '../../../../../../src/modules/course/dto/create-lesson.dto';
 import { initNestApp } from '../../../../helpers/nest-app.helper';
 import { createFakeLessonDto } from '../../../../../factories/course/dto/create-course/create-lesson.dto.factory';
+import { createFakeSectionDto } from '../../../../../factories/course/dto/create-course/create-section.dto.factory';
+import { createFakeCourseDto } from '../../../../../factories/course/dto/create-course/create-course.dto.factory';
 
 describe('Lesson Controller', () => {
   let app: INestApplication;
+  let existingCourseId: number;
+  let existingSectionId: number;
+  let existingLessonId: number;
+  const unknownSectionId = 999;
+  const unknownLessonId = 999;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -17,6 +24,30 @@ describe('Lesson Controller', () => {
     app = module.createNestApplication();
 
     await initNestApp(app);
+
+    const fakeCourse = createFakeCourseDto();
+
+    const courseResponse = await request(app.getHttpServer())
+      .post('/courses')
+      .send(fakeCourse);
+
+    existingCourseId = courseResponse.body.id;
+
+    const fakeSection = createFakeSectionDto({ courseId: existingCourseId });
+
+    const sectionResponse = await request(app.getHttpServer())
+      .post('/sections')
+      .send(fakeSection);
+
+    existingSectionId = sectionResponse.body.id;
+
+    const fakeLesson = createFakeLessonDto({ sectionId: existingSectionId });
+
+    const lessonResponse = await request(app.getHttpServer())
+      .post('/lessons')
+      .send(fakeLesson);
+
+    existingLessonId = lessonResponse.body.id;
   });
 
   afterAll(async () => {
@@ -25,16 +56,12 @@ describe('Lesson Controller', () => {
 
   describe('GET lessons/:id', () => {
     it('should return a lesson when called.', async () => {
-      const existingLessonId = 57;
-
       await request(app.getHttpServer())
         .get(`/lessons/${existingLessonId}`)
         .expect(HttpStatus.OK);
     });
 
     it('should return 404 when lesson does not exist.', async () => {
-      const unknownLessonId = 999;
-
       await request(app.getHttpServer())
         .get(`/lessons/${unknownLessonId}`)
         .expect(HttpStatus.NOT_FOUND);
@@ -48,20 +75,26 @@ describe('Lesson Controller', () => {
       errorMessage: string;
     }>([
       {
-        payload: createFakeLessonDto({ sectionId: 1 }, { name: '' }),
+        payload: createFakeLessonDto(
+          { sectionId: existingLessonId },
+          { name: '' },
+        ),
         test: 'should return 400 when name is empty.',
         errorMessage: 'name must be longer than or equal to 1 characters.',
       },
       {
-        payload: createFakeLessonDto({ sectionId: 1 }, { textContent: '' }),
+        payload: createFakeLessonDto(
+          { sectionId: existingLessonId },
+          { textContent: '' },
+        ),
         test: 'should return 400 when text content is empty.',
         errorMessage:
           'textContent must be longer than or equal to 1 characters.',
       },
       {
-        payload: createFakeLessonDto({ sectionId: 999 }),
+        payload: createFakeLessonDto({ sectionId: unknownSectionId }),
         test: 'should return 400 when section id does not exist.',
-        errorMessage: 'sectionId not found.',
+        errorMessage: 'section_id not found.',
       },
     ])('$test', async ({ payload, errorMessage }) => {
       const response = await request(app.getHttpServer())
@@ -77,7 +110,9 @@ describe('Lesson Controller', () => {
     });
 
     it('should create a lesson when called without video.', async () => {
-      const toCreateLesson = createFakeLessonDto({ sectionId: 1 });
+      const toCreateLesson = createFakeLessonDto({
+        sectionId: existingSectionId,
+      });
 
       await request(app.getHttpServer())
         .post('/lessons')
@@ -86,7 +121,9 @@ describe('Lesson Controller', () => {
     });
 
     it('should create a lesson when called with video.', async () => {
-      const toCreateLesson = createFakeLessonDto({ sectionId: 1 });
+      const toCreateLesson = createFakeLessonDto({
+        sectionId: existingSectionId,
+      });
 
       await request(app.getHttpServer())
         .post('/lessons')
