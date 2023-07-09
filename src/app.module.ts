ca32e0c05ffe63a37706b1bddb54836396751b94
Alban Thaumur <th.alban@outlook.fr>
2023-07-09 14:52:22 +0200

    :sparkles: Add a scopeGuard for authorization and a protected test route

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,18 @@ import { AppController } from './app.controller';
 import { DatabaseModule } from './modules/config/database/database.module';
 import { CourseModule } from './modules/course/course.module';
 import { CloudModule } from './modules/config/cloud/cloud.module';
+import { JwtStrategy } from './shared/auth/providers/strategies/jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { ScopeGuard } from './shared/auth/providers/guards/scope.guard';
 
 @Module({
-  imports: [DatabaseModule, CourseModule, CloudModule],
+  imports: [
+    DatabaseModule,
+    CourseModule,
+    CloudModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   controllers: [AppController],
-  providers: [],
+  providers: [JwtStrategy, ScopeGuard],
 })
 export class AppModule {}
