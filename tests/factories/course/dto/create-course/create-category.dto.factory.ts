a3b09a6c8c13b579a4127913efb3acac99f0cca2
Alban Thaumur <th.alban@outlook.fr>
2023-08-07 16:27:53 +0200

    :sparkles: Add a fake Category factory for test purposes

---
+++ tests/factories/course/dto/create-course/create-category.dto.factory.ts
@@ -0,0 +1,11 @@
+import { faker } from '@faker-js/faker';
+import { CreateCategoryDto } from '../../../../../src/modules/course/dto/create-category.dto';
+
+export const createFakeCategoryDto = (
+  category: Partial<CreateCategoryDto> = {},
+  override?: Partial<CreateCategoryDto>,
+): CreateCategoryDto => ({
+  name: category.name ?? faker.lorem.word(),
+  description: category.description ?? faker.lorem.paragraph(3),
+  ...override,
+});
