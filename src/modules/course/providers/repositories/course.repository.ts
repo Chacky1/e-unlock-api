6152cdc7eb7890eef2e21ae5a7f8615705321bf4
Alban Thaumur <th.alban@outlook.fr>
2023-08-09 17:14:11 +0200

    :sparkles: Add a route that can deliver courses of a specific category

--- src/modules/course/providers/repositories/course.repository.ts
+++ src/modules/course/providers/repositories/course.repository.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { Course } from '@prisma/client';
 import { DatabaseService } from '../../../config/database/providers/services/database.service';
 import { CreateCourseDto } from '../../dto/create-course.dto';
+import { CourseSearch } from '../services/course.service';
+import { ResourceNotFoundError } from '../../../../shared/error/types/resource-not-found.error';
 
 @Injectable()
 export class CourseRepository {
@@ -9,6 +11,31 @@ export class CourseRepository {
 
   public async findAll(): Promise<Course[]> {
     const courses = await this.databaseService.course.findMany();
+
+    return courses;
+  }
+
+  public async search(search: CourseSearch): Promise<Course[]> {
+    if (search.categoryId) {
+      const category = await this.databaseService.category.findUnique({
+        where: {
+          id: search.categoryId,
+        },
+      });
+
+      if (!category) {
+        throw new ResourceNotFoundError('CATEGORY', `${search.categoryId}`);
+      }
+    }
+
+    const courses = await this.databaseService.course.findMany({
+      where: {
+        id: search.id ?? undefined,
+        name: search.name ?? undefined,
+        description: search.description ?? undefined,
+        categoryId: search.categoryId ?? undefined,
+      },
+    });
 
     return courses;
   }
