f52c270a04297f0eacc69c50789f07cfaa6d14de
Alban Thaumur <th.alban@outlook.fr>
2023-10-22 14:11:05 +0200

    :heavy_plus_sign: Add nest/config to force env variables initialization

--- src/shared/auth/providers/strategies/jwt.strategy.ts
+++ src/shared/auth/providers/strategies/jwt.strategy.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import * as jwksRsa from 'jwks-rsa';
@@ -7,12 +8,16 @@ const { AUTH0_DOMAIN, AUTH0_AUDIENCE, AUTH0_ALGORITHM } = process.env;
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private readonly configService: ConfigService) {
+    const auth0Domain = configService.get<string>('AUTH0_DOMAIN');
+    const auth0Audience = configService.get<string>('AUTH0_AUDIENCE');
+    const auth0Algorithm = configService.get<string>('AUTH0_ALGORITHM');
+
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKeyProvider: (request, rawJwtToken, done) => {
         const client = jwksRsa({
-          jwksUri: new URL('/.well-known/jwks.json', AUTH0_DOMAIN).toString(),
+          jwksUri: new URL('.well-known/jwks.json', auth0Domain).toString(),
           cache: true,
           rateLimit: true,
           jwksRequestsPerMinute: 5,
@@ -29,9 +34,9 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
           }
         });
       },
-      audience: AUTH0_AUDIENCE,
-      issuer: AUTH0_DOMAIN,
-      algorithms: [AUTH0_ALGORITHM],
+      audience: auth0Audience,
+      issuer: auth0Domain,
+      algorithms: [auth0Algorithm],
     });
   }
 
