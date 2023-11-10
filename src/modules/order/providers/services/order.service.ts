2ce5cb6b9c5f0f7880eb09e7a21f3bb93e41a671
Alban Thaumur <th.alban@outlook.fr>
2023-11-10 09:14:35 +0100

    :sparkles: Add an update controller method to modify order status

--- src/modules/order/providers/services/order.service.ts
+++ src/modules/order/providers/services/order.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { OrderRepository } from '../repositories/order.repository';
 import { CreateOrderDto } from '../../dto/create-order.dto';
+import { UpdateOrderDto } from '../../dto/update-order.dto';
 
 @Injectable()
 export class OrderService {
@@ -8,6 +9,12 @@ export class OrderService {
 
   public async create(createOrderDto: CreateOrderDto) {
     const order = await this.orderRepository.create(createOrderDto);
+
+    return order;
+  }
+
+  public async update(orderId: number, updateOrderDto: UpdateOrderDto) {
+    const order = await this.orderRepository.update(orderId, updateOrderDto);
 
     return order;
   }
