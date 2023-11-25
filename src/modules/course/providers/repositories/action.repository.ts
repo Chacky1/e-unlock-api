b642d99b5f6ed33fbb222100de3c9725dee1067a
Alban Thaumur <th.alban@outlook.fr>
2023-11-25 19:14:34 +0100

    :sparkles: Add more informations in actions with user completion

--- src/modules/course/providers/repositories/action.repository.ts
+++ src/modules/course/providers/repositories/action.repository.ts
@@ -3,12 +3,28 @@ import { Action } from '@prisma/client';
 import { DatabaseService } from '../../../config/database/providers/services/database.service';
 import { CreateActionDto } from '../../dto/create-action.dto';
 import { SearchActionQuery } from '../../types/action.type';
+import { ResourceNotFoundError } from '../../../../shared/error/types/resource-not-found.error';
 
 @Injectable()
 export class ActionRepository {
   constructor(private readonly databaseService: DatabaseService) {}
 
   async search(filters: SearchActionQuery): Promise<Action[]> {
+    if (filters.userId) {
+      return this.databaseService.action.findMany({
+        where: {
+          lessonId: filters.lessonId,
+        },
+        include: {
+          userActions: {
+            where: {
+              userId: filters.userId,
+            },
+          },
+        },
+      });
+    }
+
     return this.databaseService.action.findMany({
       where: {
         lessonId: filters.lessonId,
